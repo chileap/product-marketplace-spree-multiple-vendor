a062bb2cf72097296dc4ca94e10d41da965e73f4
Chileap Chhin <chileapchhin@gmail.com>
2023-05-09 11:36:44 +0700

    fixed rubocop

--- app/controllers/spree/shops_controller.rb
+++ app/controllers/spree/shops_controller.rb
@@ -1,8 +1,6 @@
 module Spree
   class ShopsController < Spree::StoreController
-
-    def index
-    end
+    def index; end
 
     def show
       @shop = Spree::Vendor.find_by(slug: params[:slug])
